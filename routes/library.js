814d972bd130791b7bd55013a611d7c4d2e79234
Christopher Kemp <cmkemp52@gmail.com>
2019-12-06 04:13:48 -0500

    Temporarily changed all routes to posts for ease of use while front end is under development

--- routes/library.js
+++ routes/library.js
@@ -9,37 +9,37 @@ const {
   wishlistretreive
 } = require("../models/library");
 
-router.get("/owned/retreive", async (req, res, next) => {
+router.post("/owned/retreive", async (req, res, next) => {
   const { account, token } = req.body;
   const response = await libraryretreive(account, token);
   res.send(response);
 });
 
-router.put("/owned/add", async (req, res, next) => {
+router.post("/owned/add", async (req, res, next) => {
   const { account, token, isbn } = req.body;
   const response = await libraryadd(account, token, isbn);
   res.send(response);
 });
 
-router.delete("/owned/remove", async (req, res, next) => {
+router.post("/owned/remove", async (req, res, next) => {
   const { account, token, isbns } = req.body;
   const response = await libraryremove(account, token, isbns);
   res.send(response);
 });
 
-router.get("/wishlist/retreive", async (req, res, next) => {
+router.post("/wishlist/retreive", async (req, res, next) => {
   const { account, token } = req.body;
   const response = await wishlistretreive(account, token);
   res.send(response);
 });
 
-router.put("/wishlist/add", async (req, res, next) => {
+router.post("/wishlist/add", async (req, res, next) => {
   const { account, token, isbn } = req.body;
   const response = await wishlistadd(account, token, isbn);
   res.send(response);
 });
 
-router.delete("/wishlist/remove", async (req, res, next) => {
+router.post("/wishlist/remove", async (req, res, next) => {
   const { account, token, isbns } = req.body;
   const response = await wishlistremove(account, token, isbns);
   res.send(response);
